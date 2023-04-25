d3616cc92a02669997710f82e99c61b78fdf9396
caiocardoso418 <126572324+caiocardoso418@users.noreply.github.com>
2023-04-25 09:05:27 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -14,5 +14,11 @@ package q3
 
 func DominoPieces(m, n int) (int, error) {
 	// Seu código aqui
-	return 0, nil
+	if m <= 0 || n <= 0 {
+		return 0, fmt.Errorf("tamanho errado.")
+	}
+	produto:=m*n
+	resultado:= produto/2
+	
+	return resultado, nil
 }
