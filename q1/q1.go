240aad25e328a1c5e2c4b3000350b05b6c2116cc
caiocardoso418 <126572324+caiocardoso418@users.noreply.github.com>
2023-04-25 10:23:49 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,5 +1,7 @@
 package q1
 
+import "fmt"
+
 //Em um dia quente de verão, Pete e seu amigo Billy decidiram comprar uma melancia. Eles escolheram a maior e mais
 //saborosa, na opinião deles, e, em seguida, pesaram a fruta nas balanças, obtendo seu peso em quilos. Morrendo de sede,
 //correram para casa com a melancia e decidiram dividi-la. No entanto, enfrentaram um problema difícil.
@@ -28,7 +30,7 @@ func DivideWatermelon(weight int) (bool, error) {
 		return true, nil
 	} else if divisao == false {
 		return false, nil
-			
+
 	} else if weight == 2 {
 		return false, nil
 
